df01d9d124601c29100fe5f3d831a969bc809fd5
mhj199405 <mhj242628@outlook.com>
2021-01-28 18:40:45 +0800

    增加了Demo6_Exception

---
+++ src/com/tongtech/exception/Demo6_Exception.java
@@ -0,0 +1,12 @@
+package com.tongtech.exception;
+
+/**
+ * @author M.hj
+ * @version 1.0
+ * @date 2021/1/28 18:39
+ */
+public class Demo6_Exception {
+    public static void main(String[] args) {
+
+    }
+}
